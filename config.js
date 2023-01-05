005cb2ad874f508c80944733dc39c68326fb9860
VoidNightshade1204 <103341410+VoidNightshade1204@users.noreply.github.com>
2023-01-05 12:57:47 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ import { MdAddModerator } from "react-icons/md";
 import { SiProbot } from "react-icons/si";
 import { TbMessage2Code } from "react-icons/tb";
 const config = {
-  name: "ProBot",
+  name: "Cybernetic",
   //BotName
   inviteLink:
-    "https://discord.com/oauth2/authorize?client_id=879602399067660338&permissions=8&scope=bot&redirect_uri=https://docs.markos.ga&response_type=code",
+    "https://discord.com/api/oauth2/authorize?client_id=1057065433347395605&permissions=8&scope=bot%20applications.commands",
   //BotInvite Link
   Features: [
     {
@@ -38,10 +38,8 @@ const config = {
     // You can added more features if need add
   ],
   //Bot Features
-  serverCount: 300, //-  - Your
-  memberCount: 700, //--   - Bot
-  executedCommand: 27000, //--  - Stats
-  availableCommand: 400, //---   - Here
+  serverCount: 8, //-  - Your
+  memberCount: 119, //--   - Bot
   //Do not change if you don't know about them
   faceBookLogo: (
     <BsFacebook className="h-10 w-10 text-[#4267B2] hover:scale-125 easeIn cursor-pointer" />
@@ -54,12 +52,12 @@ const config = {
   ),
   //----------------------------------------------------------------
   // Your social handle links
-  githubLink: "https://github.com/mohitmimani",
-  twitterLink: "https://twitter.com",
-  mediumLink: "https://medium.com/",
-  facebookLink: "https://facebook.com",
-  discordLink: "https://discord.com",
-  youtubeLink: "https://www.youtube.com/channel/UCfhRHeex9lU0ZAVJot6Z2ZQ",
+  githubLink: "https://github.com/VoidNightshade1204",
+  twitterLink: "",
+  mediumLink: "",
+  facebookLink: "",
+  discordLink: "https://discord.gg/99GtyNV78m",
+  youtubeLink: "https://www.youtube.com/channel/UCw7du4gD8vqNu1R5Ii7oSQw",
 };
 
 export default config;
